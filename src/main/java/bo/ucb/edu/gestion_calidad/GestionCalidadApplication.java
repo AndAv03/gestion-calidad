1645b72dba1ab71e1f13e19635413f44186101ec
AndAv03 <andres.aviles@ucb.edu.bo>
2025-05-04 19:15:17 -0400

    initial commit

---
+++ src/main/java/bo/ucb/edu/gestion_calidad/GestionCalidadApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.gestion_calidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCalidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/edu/gestion_calidad/GestionCalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.gestion_calidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
