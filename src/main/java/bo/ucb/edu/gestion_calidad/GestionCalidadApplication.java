7106f6c8d05c877767dd1f3467e35d7e3224fde5
AndAv03 <andres.aviles@ucb.edu.bo>
2025-05-04 23:00:28 -0400

    prueba

--- src/main/java/bo/ucb/edu/gestion_calidad/GestionCalidadApplication.java
+++ src/main/java/bo/ucb/edu/gestion_calidad/GestionCalidadApplication.java
@@ -2,12 +2,19 @@ package bo.ucb.edu.gestion_calidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionCalidadApplication {
+public class GestionCalidadApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionCalidadApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestionCalidadApplication.class);
+    }
 
 }
