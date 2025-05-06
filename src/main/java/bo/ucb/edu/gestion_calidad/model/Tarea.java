8caf8b4d4e33bacdd3957ffbb406a62faae69af4
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2025-05-06 19:45:45 -0400

    funcion para mover tareas de orden.

--- src/main/java/bo/ucb/edu/gestion_calidad/model/Tarea.java
+++ src/main/java/bo/ucb/edu/gestion_calidad/model/Tarea.java
@@ -1,5 +1,6 @@
 package bo.ucb.edu.gestion_calidad.model;
 
+// Updated Tarea class with setDescripcion method
 public class Tarea {
     private int id;
     private String descripcion;
@@ -15,5 +16,9 @@ public class Tarea {
 
     public String getDescripcion() {
         return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
